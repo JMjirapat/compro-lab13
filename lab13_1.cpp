4fb996fbc03f5d828b79bac79e9a82420a423443
JMjirapat <jm_jirapat@hotmail.com>
2021-12-22 10:38:54 +0700

    Commit - Fibonanci

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int num){
+    if(num == 0)
+        return 0;
+    if(num == 1)
+        return 1;
+    return fibonacci(num-1)+fibonacci(num-2);
 }
